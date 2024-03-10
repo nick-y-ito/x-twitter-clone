ec7410914553edb730be0d3b34a4a984122a0759
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-10 16:38:16 -0700

    feat: set up vite-plugin-svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
 			},
 		},
 	},
-	plugins: [react()],
+	plugins: [react(), svgr()],
 });

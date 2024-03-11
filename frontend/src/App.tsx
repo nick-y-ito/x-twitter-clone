5e487d4d8df65fade9401b147ca21afc9af7e739
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-10 21:37:49 -0700

    feat: fetch data posts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import { Header } from "@/components/Header";
+import { Post } from "@/components/Post/Post";
+import { PostType } from "@/types/post";
+import { useEffect, useState } from "react";
 
 export const App = () => {
+	const [posts, setPosts] = useState<PostType[]>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:8080/twoots")
+			.then((res) => res.json())
+			.then(setPosts);
+	}, []);
+
 	return (
 		<>
 			<Header />
+			<main className="w-full min-h-screen">
+				{posts.map((post) => (
+					<Post key={post._id} post={post} />
+				))}
+			</main>
 		</>
 	);
 };
-

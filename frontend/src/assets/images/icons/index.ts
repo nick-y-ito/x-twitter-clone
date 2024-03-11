e92aff242c9d714c89dd706777a1ebd573bf6781
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-10 21:33:27 -0700

    feat: add icons

--- frontend/src/assets/images/icons/index.ts
+++ frontend/src/assets/images/icons/index.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite-plugin-svgr/client" />
+export { default as IconMeatball } from "./icon-meatball.svg?react";
+export { default as IconSpeechBubble } from "./icon-speech-bubble.svg?react";
 export { default as IconSettings } from "./icon-settings.svg?react";
+export { default as IconBarChart } from "./icon-bar-chart.svg?react";
+export { default as IconBookmark } from "./icon-bookmark.svg?react";
+export { default as IconBookmarkSelected } from "./icon-bookmark-selected.svg?react";
+export { default as IconHeart } from "./icon-heart.svg?react";
+export { default as IconHeartSelected } from "./icon-heart-selected.svg?react";
+export { default as IconRepost } from "./icon-repost.svg?react";
+export { default as IconRepostSelected } from "./icon-repost-selected.svg?react";
+export { default as IconShare } from "./icon-share.svg?react";
